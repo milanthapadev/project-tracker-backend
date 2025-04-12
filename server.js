6cd76cb63f6118d06e9ad92a5f3adae1525a7e8b
Milan Thapa <tmilan0604@gmail.com>
2025-04-12 12:58:15 +0545

    cors update

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://project-tracker-devagency.vercel.app'],
+  origin: [
+    'http://localhost:3000',
+    'https://project-tracker-rust-five.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
